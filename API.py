5c10ef0b5486b6b1b2dd2d15f31cf8d3743ebc4c
Tejaswini-ux237 <tejaswiniuppala8@gmail.com>
2025-06-07 10:22:06 +0530

    Add files via upload

---
+++ API.py
@@ -0,0 +1,5 @@
+def transfer_style(content_image_path, style_image_path, model_path):
+    # Dummy implementation for placeholder
+    import matplotlib.pyplot as plt
+    import numpy as np
+    return np.random.rand(224, 224, 3)
